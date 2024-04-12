73bb63fe77054a4ec12e258c27c754749a33d6b0
Shaikzaheer174 <shaikzaheer174@gmail.com>
2024-04-12 18:59:45 +0530

    datatypes

---
+++ part4/datat_types.js
@@ -0,0 +1,26 @@
+var x = 9;
+console.log(typeof x); //number
+
+var name = "javascript";
+console.log(typeof name); //string
+
+var flag = false;
+console.log(typeof flag); //boolean
+
+var undo;
+console.log(undo); //undefined
+
+var nulll = null;
+console.log(nulll); //null
+
+var names = ["Javascript", "angular", "HTML"];
+console.log(typeof names); //object
+console.log(names); //[ 'Javascript', 'angular', 'HTML' ]
+
+var personObject = {
+  // group of key value pairs we can store in
+  name: "alexa",
+  age: 18,
+  hobby: "reading books",
+};
+console.log(typeof personObject); //object
