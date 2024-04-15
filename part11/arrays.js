2bf1c195a4f81569d91f239b9184ba537bb20d94
Shaikzaheer174 <shaikzaheer174@gmail.com>
2024-04-16 01:25:12 +0530

    arrays

---
+++ part11/arrays.js
@@ -0,0 +1,34 @@
+// Arrays:
+let letters = ["a", "b", "c", "d", "e", "T"];
+// or
+let alphabets = new Array("a", "b", "c", "d", "e");
+
+// accessing elements:
+console.log(letters[5]); //T
+
+// to add elements
+letters[6] = "z";
+console.log(letters[6]); //z
+
+letters.push("d");
+console.log(letters[7]); //d
+
+letters[100] = "A";
+console.log(letters[100]); //A
+console.log(letters.length); //101
+
+console.log(letters);
+letters.pop();
+console.log(letters.length); //100
+
+letters.unshift("1");
+console.log(letters);
+
+letters.shift("1");
+console.log(letters);
+
+console.log(letters.indexOf("T")); //5
+console.log(letters.indexOf("t")); //-1 // if we dont have elements it gives -1
+
+// slice
+console.log(letters.slice(1, 4)); //[ 'b', 'c', 'd' ]
