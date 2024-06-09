bc6183cee55d7351036044446abc5c256ece42b0
Shaikzaheer174 <shaikzaheer174@gmail.com>
2024-06-09 23:03:21 +0530

    arrow functions Example

---
+++ part68/arrowFunctions.js
@@ -0,0 +1,13 @@
+// regular function:
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(1, 2); //3
+
+// arrow functions:
+let addition = (a, b) => {
+  console.log(a + b);
+};
+
+addition(1, 2); //3
